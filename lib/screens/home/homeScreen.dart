9f924c8e1ca52a0d72266525104055e32081365a
aliaqdas747 <142832510+aliaqdas747@users.noreply.github.com>
2025-02-27 16:30:04 +0500

    Task Completed

--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
